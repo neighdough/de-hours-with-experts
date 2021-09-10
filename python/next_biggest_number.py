f2bb5361c255ee7ede6b36bdd89737656e8e561c
Nate Ron-Ferguson <neighdough@gmail.com>
2021-09-10 14:33:45 -0500

    submit final solution

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 import sys
+from itertools import permutations
 
 def main():
     next_biggest_number(sys.argv[1])
 
 
 def next_biggest_number(num):
-    #TODO: Implement me!
-    return 0
+    stnum = str(num)
+    all_combo = sorted([int(''.join(i)) for i in list(permutations(list(stnum)))])
+    for v in all_combo:
+        if v > int(num):
+            return v
+    return -1
 
 if __name__ == "__main__":
     main()
